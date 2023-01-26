bbfb0e53de845f99ef2974f6cc9ecef31066f72e
hacersahin <av.hacersahin@gmail.com>
2023-01-26 23:10:43 +0300

    güncelleme

--- bootstrap-examples/session.js
+++ bootstrap-examples/session.js
@@ -6,10 +6,22 @@
 
     var username = user[0];
 
-    if (isLogin == true) {
+    if (isLogin == "true") {
         document.getElementById("dv_btn").style.display = "none";
         document.getElementById("header_sag").innerHTML = "Kullanıcı: " + username;
-        document.getElementById("logout").style.display=" ";
+        document.getElementById("logout_Btn").style.display = "block";
     }
 
+}
+
+function logout() {
+
+    var isLogin = sessionStorage.getItem("isLogin");
+
+    isLogin = false;
+
+    sessionStorage.setItem("isLogin", isLogin);
+    document.getElementById("logout_Btn").style.display = "none";
+
+    location.reload();
 }
